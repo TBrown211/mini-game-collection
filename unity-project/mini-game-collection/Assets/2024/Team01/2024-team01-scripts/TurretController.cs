7d0b239c7472818568664d27f433e2ef0d9cf99d
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-07 21:43:48 -0500

    Update TurretController.cs
    - Turret can now look up and down

--- unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/TurretController.cs
+++ unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/TurretController.cs
@@ -12,7 +12,7 @@ namespace MiniGameCollection.Games2024.Team01
 
         public Transform rotateTurret;
 
-        //float xRotation = 0f;
+        float xRotation = 0f;
 
         // Start is called before the first frame update
         void Start()
@@ -26,8 +26,10 @@ namespace MiniGameCollection.Games2024.Team01
             float lookX = ArcadeInput.Player2.AxisX * sensitivity * Time.deltaTime;
             float lookY = ArcadeInput.Player2.AxisY * sensitivity * Time.deltaTime;
 
-            //xRotation -= lookY;
+            xRotation -= lookY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             rotateTurret.Rotate(Vector3.up * lookX);
 
         }
