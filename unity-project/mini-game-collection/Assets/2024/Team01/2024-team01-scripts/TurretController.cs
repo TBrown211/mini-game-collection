bf96b0d5da4789958d72e5b5ded1e308f7187b77
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-07 21:37:27 -0500

    Turret has been added
    -Can turn along the x axis

---
+++ unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/TurretController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniGameCollection;
+
+
+namespace MiniGameCollection.Games2024.Team01
+{
+    public class TurretController : MonoBehaviour
+    {
+        public float sensitivity = 100f;
+
+        public Transform rotateTurret;
+
+        //float xRotation = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float lookX = ArcadeInput.Player2.AxisX * sensitivity * Time.deltaTime;
+            float lookY = ArcadeInput.Player2.AxisY * sensitivity * Time.deltaTime;
+
+            //xRotation -= lookY;
+
+            rotateTurret.Rotate(Vector3.up * lookX);
+
+        }
+    }
+}
+
