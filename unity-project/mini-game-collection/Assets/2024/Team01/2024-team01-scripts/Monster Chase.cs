0a44f850786afc034cb46a540a324c7c776094cf
Aiden Gibson <aidengibsonp@gmail.com>
2024-11-09 12:00:33 -0500

    New Script
    Added code that allows for an object to follow after another object with aadjustable speed

---
+++ unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/Monster Chase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterChase : MonoBehaviour
+{
+    public GameObject Monsters;
+    public GameObject Target;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Monsters.transform.position = Vector3.MoveTowards(Monsters.transform.position, Target.transform.position, speed);
+    }
+}
