144402d3c1b35dd2472599a2aede6aeda2f51d74
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-12 12:54:14 -0500

    Gun now shoots a Raycast

--- unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/TurretController.cs
+++ unity-project/mini-game-collection/Assets/2024/Team01/2024-team01-scripts/TurretController.cs
@@ -11,8 +11,14 @@ namespace MiniGameCollection.Games2024.Team01
         public float sensitivity = 100f;
 
         public Transform rotateTurret;
+        public Camera turretCam;
 
         float xRotation = 0f;
+        [SerializeField]
+        float gunRange = 200f;
+        [SerializeField]
+        float gunDamage = 25f;
+        
 
         // Start is called before the first frame update
         void Start()
@@ -23,15 +29,31 @@ namespace MiniGameCollection.Games2024.Team01
         // Update is called once per frame
         void Update()
         {
+            //Arcade Input
             float lookX = ArcadeInput.Player2.AxisX * sensitivity * Time.deltaTime;
             float lookY = ArcadeInput.Player2.AxisY * sensitivity * Time.deltaTime;
+            bool shoot = ArcadeInput.Player2.Action1.Down;
 
+            if (shoot) // Use the shoot function
+            {
+                Shoot();
+            }
+
+            //Camera Movement
             xRotation -= lookY;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
             transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             rotateTurret.Rotate(Vector3.up * lookX);
+        }
 
+        public void Shoot()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(turretCam.transform.position, turretCam.transform.forward, out hit, gunRange))
+            {
+                Debug.Log(hit.transform.name);
+            }
         }
     }
 }
